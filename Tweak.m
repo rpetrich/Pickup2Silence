faaa210411a5ade1e2892250032198054fd56b1e
Ryan Petrich <rpetrich@gmail.com>
2011-01-30 02:46:01 -0700

    Fix additional incoming calls; Hook methods at startup

--- Tweak.m
+++ Tweak.m
@@ -9,41 +9,30 @@ static SBCallAlertDisplay *callAlert;
 
 CHMethod(5, void, SBAccelerometerInterface, accelerometerDataReceived,double,received,x,float,x,y,float,y,z,float,z,type,unsigned,type)
 {
-if((x>0.3 || z> 0.1 || y>.1) && callAlert)
+	if (x>0.3 || z> 0.1 || y>.1)
+		[callAlert stopRingingOrVibrating];
+	CHSuper(5, SBAccelerometerInterface, accelerometerDataReceived,received,x,x,y,y,z,z,type,type);
+}
+
+CHMethod(0, void, SBCallAlertDisplay, alertDisplayWillBecomeVisible)
 {
-	[callAlert stopRingingOrVibrating];
-	}
-CHSuper(5, SBAccelerometerInterface, accelerometerDataReceived,received,x,x,y,y,z,z,type,type);
+	callAlert = self;
+	CHSuper(0, SBCallAlertDisplay, alertDisplayWillBecomeVisible);
 }
 
-CHMethod(0, void, SBCallAlertDisplay, finishedAnimatingIn)
+CHMethod(0, void, SBCallAlertDisplay, dealloc)
 {
-	if(!callAlert)
-	{
-	callAlert = [self retain];
-	CHLoadLateClass(SBAccelerometerInterface);
-	CHHook(5, SBAccelerometerInterface, accelerometerDataReceived,x,y,z,type);
-	}
-	
-	CHSuper(0, SBCallAlertDisplay, finishedAnimatingIn);
-}
-
-CHMethod(0, void, SBCallAlertDisplay, answerAndRelease)
-{	
-	CHSuper(0, SBCallAlertDisplay, answerAndRelease);
-}
-
-CHMethod(0, void, SBCallAlertDisplay, ignoreAndRelease)
-{	
-	CHSuper(0, SBCallAlertDisplay, ignoreAndRelease);
+	if (callAlert == self)
+		callAlert = nil;
+	CHSuper(0, SBCallAlertDisplay, dealloc);
 }
 
 CHConstructor
 {	
 	CHLoadLateClass(SBCallAlertDisplay);
-	CHHook(0, SBCallAlertDisplay, finishedAnimatingIn);
-	CHHook(0, SBCallAlertDisplay, answerAndRelease);
-	CHHook(0, SBCallAlertDisplay, ignoreAndRelease);
-	
+	CHHook(0, SBCallAlertDisplay, alertDisplayWillBecomeVisible);
+	CHHook(0, SBCallAlertDisplay, dealloc);
+	CHLoadLateClass(SBAccelerometerInterface);
+	CHHook(5, SBAccelerometerInterface, accelerometerDataReceived,x,y,z,type);
 }
 
